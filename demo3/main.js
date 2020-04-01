54730eabdbf1ce711d4f147a7ab6ae2c53af0ee6
Daniil <ddan62626@gmail.com>
2020-04-01 21:32:04 +0300

    Update main.js

--- demo3/main.js
+++ demo3/main.js
@@ -48,14 +48,14 @@ function draw(){
     
 
     clear();
-    background(255,255,255);
+    background(0,0,0);
 
 
     for(let i in Paths){
         Paths[i].Draw();
     }
 
-    stroke(255,0,0);
+    stroke(255,255,255);
 
     
 
@@ -68,7 +68,7 @@ function draw(){
         let dist = getMinDist(new Ray(p,angle,cos(angle),sin(angle)));
         //line(p.x,p.y,p.x+cos(angle)*dist,p.y-sin(angle)*dist);
         vertex(p.x+cos(angle)*dist,p.y-sin(angle)*dist);
-        fill(255,0,0);
+        fill(255,255,255);
         //circle(p.x+cos(angle)*dist,p.y-sin(angle)*dist,10);
     }
     endShape(CLOSE);
