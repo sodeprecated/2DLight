a28a679fb9bea2707ea981fb45bef1941de02510
Daniil <ddan62626@gmail.com>
2020-04-01 21:34:15 +0300

    Update RayCast.js

--- demo1/RayCast.js
+++ demo1/RayCast.js
@@ -23,7 +23,6 @@ class Line{
 
 function getDist(ray,segment){
 
-
     let r_px = ray.x,r_py = ray.y,s_px = segment.x1,s_py = segment.y1,r_dx = ray.dx,r_dy = -ray.dy,s_dx = segment.x2 - segment.x1,s_dy = segment.y2 - segment.y1;
 
     if((r_dx == s_dx && r_dy == s_dy)||(r_dx == -s_dx && r_dy == -s_dy)){
@@ -35,44 +34,14 @@ function getDist(ray,segment){
 
     let T1 = (s_px+s_dx*T2-r_px)/r_dx;
 
-
+    if(Math.abs(r_dx) <= 0.00001){
+        T1 = (s_py+s_dy*T2-r_py)/r_dy;
+    }
 
 
     if(T1 >= 0 && T2 >= -0.00000000001 && T2 <= 1.000000000001){
         return T1;
     }else return Infinity;
-
-
-
-    // RayPoint = {x:ray.x+Math.cos(ray.angle),y: ray.y-Math.sin(ray.angle)};
-    // let SL = new Line(segment.y2 - segment.y1,segment.x1 - segment.x2, segment.y1*segment.x2 - segment.y2*segment.x1);
-    // let RL = new Line(RayPoint.y - ray.y, ray.x - RayPoint.x, ray.y * RayPoint.x - RayPoint.y * ray.x);
-
-    // if(Math.abs(SL.A * RL.B - RL.A*SL.B) <= 0.000001) return Infinity;
-
-    // let IntersectPoint = {x:(SL.B * RL.C - RL.B * SL.C) / (SL.A * RL.B - RL.A * SL.B), y:(RL.A * SL.C - SL.A * RL.C) / (SL.A * RL.B - RL.A * SL.B)};
-
-
-
-    // if(IntersectPoint.x < Math.min(segment.x1,segment.x2)-0.001 || IntersectPoint.x > Math.max(segment.x1,segment.x2)+0.001) return Infinity;
-    // if(IntersectPoint.y < Math.min(segment.y1,segment.y2)-0.001 || IntersectPoint.y > Math.max(segment.y1,segment.y2)+0.001) return Infinity;
-
-
-    // let cs1 = (IntersectPoint.x - ray.x)/Math.sqrt((IntersectPoint.x - ray.x)*(IntersectPoint.x - ray.x) + (IntersectPoint.y - ray.y)*(IntersectPoint.y - ray.y));
-    // let cs2 = (RayPoint.x - ray.x)/Math.sqrt((RayPoint.x - ray.x)*(RayPoint.x - ray.x) + (RayPoint.y - ray.y)*(RayPoint.y - ray.y));
-
-
-    // let sn1 = (IntersectPoint.y - ray.y)/Math.sqrt((IntersectPoint.x - ray.x)*(IntersectPoint.x - ray.x) + (IntersectPoint.y - ray.y)*(IntersectPoint.y - ray.y));
-    // let sn2 = (RayPoint.y - ray.y)/Math.sqrt((RayPoint.x - ray.x)*(RayPoint.x - ray.x) + (RayPoint.y - ray.y)*(RayPoint.y - ray.y));
-
-
-    // if(Math.abs(cs2 - cs1) > 0.0001 || Math.abs(sn2 - sn1) > 0.0001){
-    //     return Infinity;
-    // }
-
-    // let Sqdist = (IntersectPoint.x - ray.x)*(IntersectPoint.x - ray.x) + (IntersectPoint.y - ray.y)*(IntersectPoint.y - ray.y);
-
-    // return Sqdist;
 
 }
 
